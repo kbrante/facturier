563855e5677c64e0bd345cae5ef64f8b49401b3a
karina BRANTE <karina.brante@gmail.com>
2017-06-22 16:45:49 +0200

    add profile in admin

--- factures/admin.py
+++ factures/admin.py
@@ -3,6 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import *
+from parler.admin import TranslatableStackedInline
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+
+class ProfileInline(TranslatableStackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 class ServiceInline(admin.TabularInline):
     model = Service
