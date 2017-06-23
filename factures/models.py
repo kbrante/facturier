e9f0ad8689b38f0185c474b8c55f0346843a98b4
karina BRANTE <karina.brante@gmail.com>
2017-06-23 12:12:14 +0200

    add model client

--- factures/models.py
+++ factures/models.py
@@ -17,22 +17,29 @@ class Profile(TranslatableModel):
     translation = TranslatedFields(
         bio = models.TextField( max_length=1000, verbose_name=_("Bio"))
     )
+
+class Client(models.Model):
+    clientname = models.CharField(max_length=50, null=True, blank=True, verbose_name=_("nom client"))
+    clientaddress = models.CharField(max_length=100,null=True, blank=True, verbose_name=_("adresse client"))
+    clientzipcode = models.CharField(max_length=5,null=True, blank=True, verbose_name=_("code postal client"))
+    
+
 class Status(models.Model):
     status = models.CharField(max_length=100)
 
     def __unicode__(self):
         return self.status
-        
+
     class Meta:
         verbose_name_plural=_("statuses")
 
-class Proposal(models.Model):
+class Proposition(models.Model):
     dealer = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
     customer = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
-class Service(models.Model):
+class Ligne(models.Model):
     service_name = models.CharField(max_length=200)
     unit_price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.SmallIntegerField()
-    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
+    proposal = models.ForeignKey(Proposition, on_delete=models.CASCADE)
