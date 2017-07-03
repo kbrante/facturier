19bde0f251c573f60a3b51c7f1ae6c525383cace
karina BRANTE <karina.brante@gmail.com>
2017-07-03 14:31:10 +0200

    unicode fix

--- factures/models.py
+++ factures/models.py
@@ -17,12 +17,17 @@ class Profile(TranslatableModel):
     translation = TranslatedFields(
         bio = models.TextField( max_length=1000, verbose_name=_("Bio"))
     )
+    def __unicode__(self):
+        return self.user.username
 
 class Client(models.Model):
     clientname = models.CharField(max_length=50, null=True, blank=True, verbose_name=_("nom client"))
     clientaddress = models.CharField(max_length=100,null=True, blank=True, verbose_name=_("adresse client"))
     clientzipcode = models.CharField(max_length=5,null=True, blank=True, verbose_name=_("code postal client"))
-    
+
+    def __unicode__(self):
+        return self.clientname
+
 
 class Status(models.Model):
     status = models.CharField(max_length=100)
@@ -37,6 +42,12 @@ class Proposition(models.Model):
     dealer = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.ForeignKey(Status, on_delete=models.CASCADE)
     customer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    proposition = models.ForeignKey('Ligne', on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return self.proposition.service_name
+
+
 
 class Ligne(models.Model):
     service_name = models.CharField(max_length=200)
