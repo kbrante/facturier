f382f91e3b52c3ea94f79f4f7b5da2cecd4c080d
karina BRANTE <karina.brante@gmail.com>
2017-07-06 14:01:49 +0200

    url devis-create and devis-detail

--- factures/urls.py
+++ factures/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     url(r'^(?P<slug>[\w-]+)/edit$', ProfileUpdate.as_view(), name="profile-update"),
     url(r'^(?P<slug>[\w-]+)$', ProfileDetailView.as_view(), name="profile-detail"),
     url(r'^(?P<slug>[\w-]+)/archives/$', DevisListView.as_view(), name="devis-list"),
+    url(r'^(?P<slug>[\w-]+)/create/$', DevisCreateView.as_view(), name="devis-create"),
+    url(r'^(?P<slug>[\w-]+)/archives/(?P<pk>\d+)/$', DevisDetailView.as_view(), name="devis-detail"),
+
+    # url(r'^(?P<slug>[\w-]+)/archives/(?P<pk>\d+)/$', ClientDetailView.as_view(), name="client-detail"),
+
 ]
